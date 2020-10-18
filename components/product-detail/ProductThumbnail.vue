27dfbb0858790acac71ab6de7979765ddfc2b497
George Wietor <george@wietor.com>
2020-10-18 01:24:32 -0400

    Add a special component for product thumbnail

---
+++ components/product-detail/ProductThumbnail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="w-full min-h-full h-full flex items-center justify-center">
+    <img
+      v-if="hasImage"
+      :src="src"
+      class="object-contain p-4"
+    >
+    <p
+      v-if="!hasImage"
+      class="text-center"
+    >There is no image available.</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductThumbnail',
+    props: {
+        src: {
+            type: String, 
+            required: true,
+        }
+    },
+    computed: {
+        hasImage() {
+            return this.src;
+        }
+  }
+}
+</script>
